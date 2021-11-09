c15133baa652ebbad4f626e65245527235b23bc6
lumengjuan <lumengjuan@5i5j.com>
2021-11-09 13:56:58 +0800

    :sparkles:新增深拷贝功能

--- modules/index.ts
+++ modules/index.ts
@@ -2,5 +2,6 @@ const version: string = require("../package.json").version;
 import { formatTime } from "../modules/formatTime"
 import { regExpr } from "../modules/regExpr"
 import { ArrayTool } from "../modules/arrayExtension"
+import { deepClone } from "../modules/deepClone"
 
-export { version, formatTime, regExpr, ArrayTool };
+export { version, formatTime, regExpr, ArrayTool, deepClone };
