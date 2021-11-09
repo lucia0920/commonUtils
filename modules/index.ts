8dd097bda01eba96d86ffcbbd07b02f31876f993
lumengjuan <lumengjuan@5i5j.com>
2021-11-09 13:26:46 +0800

    :sparkles:新增数组操作方法

--- modules/index.ts
+++ modules/index.ts
@@ -1,5 +1,6 @@
 const version: string = require("../package.json").version;
 import { formatTime } from "../modules/formatTime"
 import { regExpr } from "../modules/regExpr"
+import { ArrayTool } from "../modules/arrayExtension"
 
-export { version, formatTime, regExpr };
+export { version, formatTime, regExpr, ArrayTool };
